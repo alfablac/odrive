b99b5cedaee0335a02cc2507acb9d8e84b13cd44
alfablac <likao89@hotmail.com>
2020-06-11 09:31:27 -0300

    Update odrive.py
    adds support bit.ly and file links

--- odrive.py
+++ odrive.py
@@ -1,23 +1,36 @@
 import requests
+import bs4
 import re
 import json
 import subprocess
+import argparse
 
-BAIXAR = input("Enter URL: ")
+COOKIE_BASE = "DOMAIN	FALSE	/	TRUE	0	FedAuth	AUTH    \n     DOMAIN	FALSE	/	FALSE	0	FeatureOverrides_enableFeatures	 \n   DOMAIN	FALSE	/	FALSE	0	FeatureOverrides_disableFeatures"
+parser = argparse.ArgumentParser(
+    description='odrive sharepoint file/folder downloader'
+)
+parser.add_argument('-u', '--url', help='url para download', required=True)
+args = parser.parse_args()
+BAIXAR = args.url
+
+if 'bit.ly' in BAIXAR:
+    extract_url = requests.get(url=BAIXAR)
+    BAIXAR = extract_url.history[0].headers['location']
+
+print("Baixando URL:")
+print(BAIXAR)
 auth_url = requests.get(url=BAIXAR)
 
 HEADERS_COOKIES = auth_url.headers['Set-Cookie']
 PCBROWSER_UA = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+AUTH = re.search('(FedAuth=)(.+?)(\;)', HEADERS_COOKIES, re.IGNORECASE).group(2)
+DOMAIN = re.search('(http(s?):\/\/)(.+?)(\/)', BAIXAR, re.IGNORECASE).group(3)
 
-HEADERS = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-    'User-Agent': PCBROWSER_UA,
-    'Set-Cookie': HEADERS_COOKIES,
-    'Referer': BAIXAR,
+COOKIE_BASE = re.sub(r"DOMAIN", DOMAIN, COOKIE_BASE)
+COOKIE_BASE = re.sub(r"AUTH", AUTH, COOKIE_BASE)
 
-}
+with open('cookies.txt', 'w') as f:
+    f.write(COOKIE_BASE)
 
 HEADERS_JSON = {
     'Accept': 'application/json;odata=verbose',
@@ -31,14 +44,17 @@ HEADERS_JSON = {
     'Referer': BAIXAR,
 }
 print("Fazendo request para autorização")
-page_iq = requests.get(url=auth_url.url, headers=HEADERS, cookies=auth_url.cookies)
+page_iq = requests.get(url=auth_url.url, headers=HEADERS_JSON, cookies=auth_url.cookies)
 page_data = re.search('(var g_listData = )({.*})(\;)', page_iq.text, re.IGNORECASE | re.DOTALL).group(2)
 page_payload = re.search('(var g_payload = )({.*})(\;)', page_iq.text, re.IGNORECASE).group(2)
 page_payload_json = json.loads(page_payload)
 page_data_json = json.loads(page_data)
 uniqueid_list = []
 HTTP_ROOT = page_data_json['HttpRoot']
-ROOT_FOLDER = page_data_json['rootFolder']
+if len(page_data_json['ListData']['Row']) > 0:
+    ROOT_FOLDER = page_data_json['rootFolder']
+else:
+    ROOT_FOLDER = re.search('^(.+)\/([^/]+)$', page_data_json['rootFolder'], re.IGNORECASE).group(1)
 ARROBA_1 = page_data_json['ListUrl']
 API_URL = HTTP_ROOT + '/_api/web/GetListUsingPath(DecodedUrl=@a1)/RenderListDataAsStream?@a1=\'{}\'&RootFolder={}&TryNewExperienceSingle=TRUE'.format(ARROBA_1, ROOT_FOLDER)
 API_URL_REP = HTTP_ROOT + '/_api/web/GetListUsingPath(DecodedUrl=@a1)/RenderListDataAsStream?@a1=\'{}\'&TryNewExperienceSingle=TRUE'.format(ARROBA_1)
@@ -72,7 +88,7 @@ with open('download_list.txt', 'w') as f:
 print("Chamando aria2c")
 subprocess.call(["aria2c", "--dir=./", "--input-file=download_list.txt",
                  "--load-cookies=cookies.txt","--max-concurrent-downloads=1", "--connect-timeout=60",
-                 "--max-connection-per-server=16", "--split=16", "--min-split-size=1M",
+                 "--max-connection-per-server=16", "--continue=true", "--split=16", "--min-split-size=1M",
                  "--human-readable=true", "--download-result=full", "--file-allocation=none"])
 
 print(download_list)
